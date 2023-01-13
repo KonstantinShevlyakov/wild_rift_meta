9aa47ff05d4118046f2c32038611dbd9105ee268
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2023-01-13 19:47:36 +0300

    meta added

--- wildrift/scripts/wildrift_bot.py
+++ wildrift/scripts/wildrift_bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+from wildrift.scripts.get_meta import get_splus_tier
 
 TOKEN =
 
@@ -12,5 +13,9 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix='$')
 async def test(ctx, arg):
     await ctx.send(arg)
     
-    
+
+@bot.command(pass_context=True)
+async def meta(ctx):
+    await ctx.send(get_splus_tier())
+ 
 bot.run(TOKEN)
