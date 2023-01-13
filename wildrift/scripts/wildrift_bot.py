7c13a1e67b1ded10bb979dd93410e04c48844802
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2023-01-13 19:39:59 +0300

    all changes

--- wildrift/scripts/wildrift_bot.py
+++ wildrift/scripts/wildrift_bot.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python3
 import discord
 from discord.ext import commands
+import asyncio
 
-TOKEN = 'MTA2MzQ5MTkxODk1ODQ0NDU2NQ.GGO5iK.JGKWAuI0rlKn9YGtwZnbaJHJ4eFA3sPPjrtABo'
+TOKEN = #'MTA2MzQ5MTkxODk1ODQ0NDU2NQ.GGO5iK.JGKWAuI0rlKn9YGtwZnbaJHJ4eFA3sPPjrtABo'
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='$')
 
 @bot.command (pass_context=True)
 async def test(ctx, arg):
