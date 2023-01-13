706926921c11bb3b7816cbc476276db8748ba459
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2023-01-13 19:53:16 +0300

    changes

--- wildrift/scripts/wildrift_bot.py
+++ wildrift/scripts/wildrift_bot.py
@@ -3,19 +3,13 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
-from wildrift.scripts.get_meta import get_splus_tier
 
-TOKEN =
+TOKEN = 'TOKEN'
 
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix='$')
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
 
 @bot.command (pass_context=True)
 async def test(ctx, arg):
     await ctx.send(arg)
-    
 
-@bot.command(pass_context=True)
-async def meta(ctx):
-    await ctx.send(get_splus_tier())
- 
 bot.run(TOKEN)
